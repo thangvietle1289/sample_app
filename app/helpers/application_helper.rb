af47d6b7e01e3bc69710a2788c352f5fdf47fca2
Thang Le <tleviet@openwt.com>
2016-02-24 11:21:15 +0700

    add unit test for controller and update title web page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # Return the full title on a per-page basic.
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+
+    # base_title if page_title.empty?
+    # page_title + " | " + base_title
+
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
